a0ce829b55d27e9bf248add9fa7e578e74f1f37f
Kushal Banda <kushalbanda265@gmail.com>
2025-12-25 11:59:43 +0530

    Add main application entry point with FastAPI setup and router inclusion

---
+++ server/main.py
@@ -0,0 +1,14 @@
+"""FastAPI application entrypoint."""
+
+from utils.pathing import standardize_sys_path
+
+standardize_sys_path()
+
+from fastapi import FastAPI
+from server.api.routers.github_search_router import (
+    router as github_search_router,
+)
+
+app = FastAPI(title="CommitRecap API")
+
+app.include_router(github_search_router)
